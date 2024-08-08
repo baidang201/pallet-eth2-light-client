aaa5637ebc1282b1caa64d80c2cac28f12960a9c
Liquid369 <Liquid369@gmail.com>
2024-08-08 07:58:16 -0500

    Harden config options

--- crates/eth2-pallet-init/src/substrate_pallet_client.rs
+++ crates/eth2-pallet-init/src/substrate_pallet_client.rs
@@ -24,15 +24,18 @@ use webb_proposals::TypedChainId;
 
 #[cfg(feature = "brooklyn")]
 #[subxt::subxt(runtime_metadata_path = "./metadata/ggxruntime_brooklyn.scale")]
-pub mod tangle {}
+pub mod tangle;
 
-#[cfg(feature = "sydney")]
-#[subxt::subxt(runtime_metadata_path = "./metadata/ggxruntime_sydney.scale")]
-pub mod tangle {}
-
-#[cfg(feature = "toronto")]
+#[cfg(all(not(feature = "brooklyn"), feature = "toronto"))]
 #[subxt::subxt(runtime_metadata_path = "./metadata/ggxruntime_toronto.scale")]
-pub mod tangle {}
+pub mod tangle;
+
+#[cfg(all(not(feature = "brooklyn"), not(feature = "toronto"), feature = "sydney"))]
+#[subxt::subxt(runtime_metadata_path = "./metadata/ggxruntime_sydney.scale")]
+pub mod tangle;
+
+#[cfg(all(not(feature = "brooklyn"), not(feature = "toronto"), not(feature = "sydney")))]
+compile_error!("One of the features 'brooklyn', 'sydney', or 'toronto' must be enabled.");
 
 use tangle::runtime_types::pallet_eth2_light_client;
 
@@ -459,3 +462,4 @@ fn get_sr25519_keys_from_suri<T: AsRef<str>>(suri: T) -> anyhow::Result<Pair> {
 		}
 	}
 }
+
