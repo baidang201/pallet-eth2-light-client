2d861ca30b07a47d67cd425ef71ccaf136f0ee97
Liquid369 <Liquid369@gmail.com>
2024-08-09 10:58:39 -0500

    Fix mod

--- crates/eth2-pallet-init/src/substrate_pallet_client.rs
+++ crates/eth2-pallet-init/src/substrate_pallet_client.rs
@@ -447,11 +447,11 @@ fn get_sr25519_keys_from_suri<T: AsRef<str>>(suri: T) -> anyhow::Result<Pair> {
 
 #[cfg(all(feature = "brooklyn", not(feature = "sydney")))]
 #[subxt::subxt(runtime_metadata_path = "./metadata/ggxruntime_brooklyn.scale")]
-pub mod tangle;
+pub mod tangle {}
 
 #[cfg(all(not(feature = "brooklyn"), feature = "sydney"))]
 #[subxt::subxt(runtime_metadata_path = "./metadata/ggxruntime_sydney.scale")]
-pub mod tangle;
+pub mod tangle {}
 
 #[cfg(all(not(feature = "brooklyn"), not(feature = "sydney")))]
 compile_error!("One of the features 'brooklyn' or 'sydney' must be enabled.");
